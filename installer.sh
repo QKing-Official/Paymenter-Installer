2cffc21c48078632ccb5046840c7d28e8585eec4
QKing <162024807+QKing-Official@users.noreply.github.com>
2025-08-20 21:05:59 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,8 +6,10 @@ echo "  / _ \___ ___ ____ _  ___ ___  / /____ ____  /  _/__  ___ / /____ _/ / /_
 echo " / ___/ _ \`/ // /  ' \/ -_) _ \/ __/ -_) __/ _/ // _ \(_-</ __/ _ \`/ / / -_) __/"
 echo "/_/   \_,_/\_, /_/_/_/\__/_//_/\__/\__/_/   /___/_//_/___/\__/\_,_/_/_/\__/_/   "
 echo "          /___/                                                                  "
+echo "BETA"
 echo "By QKing"
 
+IP=$(curl -s https://api.ipify.org)
 # License agreement
 echo "Using this script you agree to the license and use it at your own risk."
 read -p "Do you accept? (y/N): " agree
@@ -38,7 +40,18 @@ echo
 read -p "Enter your Paymenter database name [paymenter]: " DB_NAME
 DB_NAME=${DB_NAME:-paymenter}
 
-read -p "Enter your Paymenter domain/IP used for the Webserver configuration (without https://): " APP_URL
+read -p "Enter your Paymenter domain/IP used for the Webserver configuration (without http(s)://): " APP_URL
+
+read -p "[BETA! Use on your own risk!] Enable SSL? (For domain only! For ssl make an A record to $IP (y/N): " SSL
+
+# Default to "N" if empty
+SSL=${SSL:-N}
+
+if [[ "$SSL" == "y" || "$SSL" == "Y" ]]; then
+    SSL="Y"
+else
+    SSL="N"
+fi
 
 # Update system and install dependencies
 echo "Installing dependencies..."
@@ -82,18 +95,47 @@ composer install --no-dev --optimize-autoloader
 
 # Setup database
 echo "Setting up database..."
-mysql -u root <<MYSQL_SCRIPT
-CREATE USER IF NOT EXISTS '$DB_USER'@'127.0.0.1' IDENTIFIED BY '$DB_PASS';
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'127.0.0.1' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-MYSQL_SCRIPT
+
+# Check if user exists
+USER_EXISTS=$(mysql -u root -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$DB_USER');")
+if [ "$USER_EXISTS" -eq 1 ]; then
+    read -p "User '$DB_USER' already exists. Remove user and associated database? [y/N] " response
+    response=${response:-N}
+    if [[ "$response" =~ ^[Yy]$ ]]; then
+        echo "Removing user and database..."
+        mysql -u root -e "DROP USER '$DB_USER'@'127.0.0.1';"
+        mysql -u root -e "DROP DATABASE IF EXISTS $DB_NAME;"
+    else
+        echo "Aborting setup."
+        exit 1
+    fi
+fi
+
+# User Creation
+mysql -u root -e "CREATE USER '$DB_USER'@'127.0.0.1' IDENTIFIED BY '$DB_PASS';"
+if [ $? -ne 0 ]; then
+    echo "Error creating user '$DB_USER'."
+    exit 1
+fi
+
+# Database Creation
+mysql -u root -e "CREATE DATABASE $DB_NAME;"
+if [ $? -ne 0 ]; then
+    echo "Error creating database '$DB_NAME'."
+    exit 1
+fi
+
+# Finish the database setup
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'127.0.0.1' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+echo "Database '$DB_NAME' and user '$DB_USER' set up successfully."
+
 
 # Setup .env
 echo "Setting up the .env..."
 cp .env.example .env
 php artisan key:generate --force
-php artisan storage:link
+[ -L public/storage ] && rm public/storage; php artisan storage:link
 
 # Update .env with user info
 sed -i "s/DB_DATABASE=.*/DB_DATABASE=$DB_NAME/" .env
@@ -141,8 +183,53 @@ systemctl enable --now redis-server
 
 # Nginx setup
 echo "Setting up Nginx configuration..."
-echo "Type: Non-SSL (currently only supported)"
-echo "For SSL please manually change this"
+echo "SSL: $SSL"
+
+if [[ "$SSL" != "y" && "&SSL" != "Y" ]]; then
+cat <<EOF >/etc/nginx/sites-available/paymenter.conf
+server {
+    listen 80;
+    listen [::]:80;
+    server_name $APP_URL;
+    return 301 https://$host$request_uri;
+}
+
+server {
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+    server_name $APP_URL;
+    root /var/www/paymenter/public;
+
+    index index.php;
+
+    ssl_certificate /etc/letsencrypt/live/$APP_URL/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$APP_URL/privkey.pem;
+
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/var/run/php/php8.3-fpm.sock;
+    }
+}
+
+EOF
+sudo apt install -y python3-certbot-nginx
+sudo systemctl stop nginx
+PID=$(sudo lsof -t -i :80)
+if [ -n "$PID" ]; then
+    sudo kill $PID
+    echo "Killed process $PID on port 80"
+else
+    echo "No process found on port 80"
+fi
+
+sudo systemctl stop nginx
+certbot certonly --nginx -d $APP_URL
+(crontab -l 2>/dev/null; echo "0 23 * * * certbot renew --quiet --deploy-hook 'systemctl restart nginx'") | crontab -
+else
 cat <<EOF >/etc/nginx/sites-available/paymenter.conf
 server {
     listen 80;
@@ -161,7 +248,9 @@ server {
         fastcgi_pass unix:/var/run/php/php8.3-fpm.sock;
     }
 }
+
 EOF
+fi
 
 ln -s /etc/nginx/sites-available/paymenter.conf /etc/nginx/sites-enabled/
 sudo rm -f /etc/nginx/sites-enabled/default
@@ -169,10 +258,13 @@ sudo rm -f /etc/nginx/sites-available/default
 sudo rm -f /etc/nginx/conf.d/default.conf
 systemctl restart nginx
 echo "Nginx configuration succesfull"
+echo "SSL: $SSL"
 
 # Set correct permissions
 echo "Setting the correct permissions"
 chown -R www-data:www-data /var/www/paymenter/*
+chmod -R 755 storage/* bootstrap/cache/
 
 echo "Installation Complete!"
+echo "If this doesn't work contact me on discord. Username: qkingsoftware
 echo "Visit your app at http://$APP_URL"
